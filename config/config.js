180811cf7eeea3e935066de68b826577b472c882
jemin <jemin03120111@gmail.com>
2021-09-14 18:15:14 +0900

    수정버전

--- config/config.js
+++ config/config.js
@@ -3,21 +3,21 @@ require('dotenv').config()
 module.exports = {
   development: {
     username: "root",
-    password: "71312m**0312",
+    password: process.env.SEQUELIZE_PASSWORD,
     database: "nodebird_schema",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   test: {
     username: "root",
-    password: null,
+    password: process.env.SEQUELIZE_PASSWORD,
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   production: {
     username: 'root',
-    password: '71312m**0312',
+    password: process.env.SEQUELIZE_PASSWORD,
     database: 'nodebird_schema',
     host: '127.0.0.1',
     dialect: 'mysql',
